7a73e243bd64ebbc40b74c16b4af3b84e55b23e4
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2024-11-19 22:37:27 +0530

    routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
-export const register =(req,res)=>{
-    //eval 
+
+export const register = async (req,res)=>{
+    // const {username,email,password}=(req.body);
+    // const hashedPassword =await bcrypt.hash(password, 10);
+    // console.log(hashedPassword);
     console.log(req.body);
 }
 export const login =(req,res)=>{
