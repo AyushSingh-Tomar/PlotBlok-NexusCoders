1ba6855d495f3e97fdfffeb5c3b96e501b85ea5f
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2024-11-20 06:37:55 +0530

    LoginConstraints

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,25 @@
-
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+import prisma from "../lib/prisma.js"
 export const register = async (req,res)=>{
-    // const {username,email,password}=(req.body);
-    // const hashedPassword =await bcrypt.hash(password, 10);
-    // console.log(hashedPassword);
-    console.log(req.body);
+    const {username,email,password}=(req.body);
+    try{
+    const hashedPassword =await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    const newUser=await prisma.user.create({
+        data:{
+            username,
+            email,
+            password: hashedPassword,
+        },
+    });
+    console.log(newUser);
+    res.status(201).json({message: "User created Successfully"});
+}
+catch(err){console.log(err);
+    res.status(201).json({message: "Failed create user"});
+
+}
 }
 export const login =(req,res)=>{
     //eval 
